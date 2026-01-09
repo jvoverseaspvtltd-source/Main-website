10b6b3478cd71407efc9b8c5b83e810b3afed8fb
JV Overseas <info@jvoverseas.com>
2026-01-09 17:00:55 +0530

    codejh

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'jvoverseas.com',
+          },
+        ],
+        destination: 'https://www.jvoverseas.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
